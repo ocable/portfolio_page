d830a47aa3cf0012aedbb52932f1f64d73fb2972
Orion Cable <noiro.elbac@gmail.com>
2024-09-14 11:22:14 -0400

    updtated to a mailto link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,26 @@
 import React, { useState } from "react";
 
 export default function Contact() {
-  const [isCopied, setIsCopied] = useState(false);
-  const email = "orionfcable@gmail.com";
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(email);
-    setIsCopied(true);
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 1500);
-  };
-
   return (
     <div className="flex flex-col items-center mb-80">
       <h1 className="text-5xl font-bold mt-40">Contact</h1>
-    
+
       <section className="flex flex-row mt-32">
-        <div onClick={() => navigator.clipboard.writeText(email)} className="flex flex-col items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="80"
-            height="80"
-            viewBox="0 0 24 24"
-            className="mx-20"
-          >
-            <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
-          </svg>
-          <h2 className="mt-4 text-xl">Email</h2>
-        </div>
-    
-    
+        <a href="mailto:orionfcable@gmail.com">
+          <div className="flex flex-col items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="80"
+              height="80"
+              viewBox="0 0 24 24"
+              className="mx-20"
+            >
+              <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
+            </svg>
+            <h2 className="mt-4 text-xl">Email</h2>
+          </div>
+        </a>
+
         <a href="https://github.com/ocable">
           <div className="flex flex-col items-center">
             <svg
