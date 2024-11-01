e2a647adc016bafec3b5961fe974b7c833b61c2f
Orion Cable <orioncable@Orions-Air.lan>
2024-10-31 22:31:02 -0400

    added new proj

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -63,37 +63,64 @@ export default function Projects() {
         <div className="group/card dark:md:hover:bg-gray-700 md:hover:bg-blue-100 mx-10 md:mx-4 md:p-4 rounded-2xl flex flex-row items-start my-10">
           <section className=" flex flex-col">
             <h2 className="text-3xl font-bold text-nowrap mr-10 group-hover/card:text-blue-500">
-              Buoy Data Visualizer
+              Surf Forecast Web App
             </h2>
             <p className="my-2">
-              A Python program that visualizes raw spectral buoy data from the
-              NBDC. Plots and uses PSD data to calculate significant wave height
-              and period. A breakdown of each swell component is also provided.
+              A Python backend that pulls raw spectral buoy data from the NBDC.
+              React frontend displays live buoy readings as well as forecasted
+              wave data from the GFS model.
             </p>
             <div className="flex flex-row">
               <h3 className="max-w-min font-bold dark:bg-gray-700 bg-blue-100 ml-0 m-1 px-2 py-0.5 rounded-2xl">
                 Python
+              </h3>{" "}
+              <h3 className="max-w-min font-bold dark:bg-gray-700 bg-blue-100 ml-0 m-1 px-2 py-0.5 rounded-2xl">
+                React
               </h3>
             </div>
-            <section className="group/link flex flex-row text-nowrap w-min">
-              <h4 className="text-xl font-bold mt-2 hover:text-blue-500">
-                <a href="https://github.com/ocable/wave_math" target="_blank">
-                  View on GitHub
-                </a>
-              </h4>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-                className="w-5 h-5 ml-0.5 mb-0.5 self-end group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:fill-blue-500 dark:fill-white"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </section>
+            <div className="flex flex-row">
+              <section className="group/link flex flex-row text-nowrap w-min mr-4">
+                <h4 className="text-xl font-bold mt-2 hover:text-blue-500">
+                  <a href="https://github.com/ocable/wave_math" target="_blank">
+                    View backend on GitHub
+                  </a>
+                </h4>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                  className="w-5 h-5 ml-0.5 mb-0.5 self-end group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:fill-blue-500 dark:fill-white"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </section>
+              <section className="group/link flex flex-row text-nowrap w-min">
+                <h4 className="text-xl font-bold mt-2 hover:text-blue-500">
+                  <a
+                    href="https://github.com/ocable/wave_frontend"
+                    target="_blank"
+                  >
+                    View frontend on GitHub
+                  </a>
+                </h4>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                  className="w-5 h-5 ml-0.5 mb-0.5 self-end group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:fill-blue-500 dark:fill-white"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </section>
+            </div>
           </section>
         </div>
 
